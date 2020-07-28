4f8eff91a9097a582eb080bcad55833410357640
jiwon79 <59159410+jiwon79@users.noreply.github.com>
2020-07-29 01:33:52 +0900

    amai web - first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Amai Web</title>
+</head>
+<body>
+    <h1>amai web</h1>
+    <ul>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">Gallay</a></li>
+        <li><a href="#">Community</a></li>
+        <li><a href="Views/login.php">Log In</a></li>
+    </ul>
+
+    
+</body>
+</html>
