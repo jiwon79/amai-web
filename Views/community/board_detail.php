ef251b0188edf308fb1c603d68c1e271a5975220
jiwon79 <59159410+jiwon79@users.noreply.github.com>
2020-08-14 03:53:54 +0900

    board_detail 페이지

--- Views/community/board_detail.php
+++ Views/community/board_detail.php
@@ -6,6 +6,33 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>board detail</h1>
+    <?php
+    include "../../php/db.php";
+
+    $board_no = $_GET["board_no"];
+    echo $board_no."번째 글 내용<br>";
+
+    $sql = "SELECT board_no, board_title, board_content, board_user, board_date FROM board WHERE board_no = ".$board_no."";
+    $result = mysqli_query($conn,$sql);
+    if($result) {
+        echo "조회성공";
+    } else {
+        echo "결과없음 : ".mysqli_error($conn);
+    }
+    ?>
+    <br>
+
+    <?php
+    $row = mysqli_fetch_array($result);
+    ?>
+    
+        글 제목 : <?php echo $row["board_title"]; ?><br>
+        작성자 : <?php echo $row["board_user"]; ?><br>
+        작성 일자 : <?php echo $row["board_date"]; ?><br>
+        글 내용 : <?php echo $row["board_content"]; ?><br>
+
+    <?php
+    
+    ?>
 </body>
 </html>
